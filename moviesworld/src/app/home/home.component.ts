f95fc5a15c1b5ddeaf52b23ec2b7917de351f023
unknown <rohitratate855@gmail.com>
2025-05-15 12:16:08 +0530

    Home Page completed

--- moviesworld/src/app/home/home.component.ts
+++ moviesworld/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Route, Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router:Router) { }
   trendingMovies: any;
   theatreMovies:any;
   popularMovies:any;
@@ -36,5 +37,10 @@ export class HomeComponent implements OnInit {
       this.popularMovies = movies;
       console.log(this.popularMovies)
     })
+  }
+
+  goToMovie(type:string, id:string){
+    this.router.navigate(['movie', type, id]);
+
   }
 }
