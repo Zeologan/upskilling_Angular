00c19dc2f192fef0646eb356f9f3e9b6f2df4662
unknown <rohitratate855@gmail.com>
2025-05-17 12:02:32 +0530

    still need to render star

--- moviesworld/src/app/feature/star-rating/star-rating.component.ts
+++ moviesworld/src/app/feature/star-rating/star-rating.component.ts
@@ -6,7 +6,19 @@ import { ChangeDetectorRef, Component, Input } from '@angular/core';
   styleUrls: ['./star-rating.component.css']
 })
 export class StarRatingComponent {
-  @Input() rating: any;
+  // @Input() rating: number = 0;
   @Input() isReadOnly : boolean = false;
 
+private _rating: number = 0;
+
+@Input() set rating(value: number) {
+  this._rating = value;
+  this.roundedRating = Math.round(value); // You can also use Math.floor() or Math.ceil()
+}
+
+get rating() {
+  return this._rating;
+}
+
+roundedRating = 0;
 }
--- moviesworld/src/app/movie/movie.component.ts
+++ moviesworld/src/app/movie/movie.component.ts
@@ -7,37 +7,51 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
-export class MovieComponent implements OnInit{
+export class MovieComponent implements OnInit {
   type = "";
   id = "";
   url = "";
-  movies:any;
-  movie:any;
+  movies: any;
+  movie: any;
   // review:any;
 
-  constructor(private route:ActivatedRoute,private http:HttpClient){}
-  ngOnInit():void{
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  ngOnInit(): void {
     this.type = this.route.snapshot.params['type'];
     this.id = this.route.snapshot.params['id'];
-    if(this.type === 'trending'){
+    if (this.type === 'trending') {
       this.url = 'http://localhost:4200/assets/data/trending-movies.json';
     }
-    if(this.type === 'theatre'){
+    if (this.type === 'theatre') {
       this.url = 'http://localhost:4200/assets/data/theatre-movies.json';
     }
-    if(this.type === 'popular'){
+    if (this.type === 'popular') {
       this.url = 'http://localhost:4200/assets/data/popular-movies.json';
     }
     this.getMovie();
   }
 
-  getMovie(){
-    this.http.get(this.url).subscribe((movies)=>{
-      this.movies = movies
+  getMovie() {
+    this.http.get(this.url).subscribe((movies: any) => {
+      this.movies = movies;
       let index = this.movies.findIndex((movie: { id: string; }) => movie.id == this.id);
-      if(index> -1){
-        this.movie = this.movies[index]
+      if (index > -1) {
+        this.movie = this.movies[index];
+
+        // ✅ Convert rating to number
+        this.movie.rating = parseFloat(this.movie.rating);
+        console.log('Movie rating (after parseFloat):', this.movie.rating, typeof this.movie.rating);
+
+
+        // ✅ Convert each review rating to number
+        if (this.movie.reviews?.length > 0) {
+          this.movie.reviews = this.movie.reviews.map((review: any) => ({
+            ...review,
+            rating: parseFloat(review.rating)
+          }));
+        }
       }
-    })
+    });
   }
 }
+
