9f8cb753c9af4576b4c06d772a59f507dba3dbc9
unknown <rohitratate855@gmail.com>
2025-05-23 17:21:55 +0530

    todo

---
+++ angular-todo/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+export interface TodoList{
+    id:number;
+    task:string;
+    completed:boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  todoList : TodoList [] = [];
+  newTask:string = '';
+
+  addTask():void{
+    if(this.newTask.trim() !== ''){
+      const newTodoItem: TodoList = {
+        id : Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+
+      this.todoList.push(newTodoItem);
+      console.log(this.todoList)
+      this.newTask = '';
+    }
+  }
+
+  toggleCompleted(index:number):void{
+    this.todoList[index].completed = !this.todoList[index].completed;
+  }
+
+  deleteTask(id:number):void{
+    this.todoList = this.todoList.filter(item => item.id !== id);
+    console.log(this.todoList);
+  }
+}
+
+
+
